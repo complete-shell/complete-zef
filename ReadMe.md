1078336cf8b72b3f40f8286e527f83a248c7e9d7
Ingy döt Net <ingy@ingy.net>
2019-01-19 22:42:52 -0800

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+complete-template
+=================
+
+This is the complete-shell compdef package for the TEMPLATE command.
---
+++ zef.bash
@@ -0,0 +1,11 @@
+name() {
+  hint 'Short name or spec of CompUnit::Repository to install to'
+}
+
+ecosystem() {
+  hint 'enter an ecosystem module name'
+}
+
+module() {
+  hint 'Enter a Perl6 module name'
+}
